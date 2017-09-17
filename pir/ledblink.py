e28497214d962b0dfe314b879d9c9fe673fc8bba
Chris Warren <cmwarren@gmail.com>
2017-09-17 12:22:20 +0100

    telecare demo

--- pir/ledblink.py
+++ pir/ledblink.py
@@ -5,8 +5,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3,GPIO.OUT)     #Define pin 3 as an output pin
 
 while True:
+    try:
         GPIO.output(3,1)   #Outputs digital HIGH signal (5V) on pin 3
         time.sleep(1)      #Time delay of 1 second
 
         GPIO.output(3,0)   #Outputs digital LOW signal (0V) on pin 3
         time.sleep(1)      #Time delay of 1 second
+        
+    except KeyboardInterrupt:
+            print "Stopping..."
+            break
+            
+GPIO.cleanup()
--- pir/pirtest.py
+++ pir/pirtest.py
@@ -5,12 +5,19 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
 while True:
+    try:
        i=GPIO.input(11)
        if i==0:                 #When output from motion sensor is LOW
-             print "No intruders",i
+             print "No movement",i
              GPIO.output(3, 0)  #Turn OFF LED
-             time.sleep(0.1)
+             time.sleep(1)
        elif i==1:               #When output from motion sensor is HIGH
-             print "Intruder detected",i
+             print "Movement detected",i
              GPIO.output(3, 1)  #Turn ON LED
-             time.sleep(0.1)
+             time.sleep(1)
+    
+    except KeyboardInterrupt:
+            print "Stopping..."
+            break
+            
+GPIO.cleanup()
